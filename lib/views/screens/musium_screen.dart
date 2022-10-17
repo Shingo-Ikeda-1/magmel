9dfde6423e5ac8c65f761edf253de4a1f9c357d6
Shingo Ikeda <ikdshingo@gmail.com>
2022-10-17 17:44:01 +0900

    First Creation

---
+++ lib/views/screens/musium_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MusiumScreen extends StatelessWidget {
+  const MusiumScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        // child: GestureDetector(onHorizontalDragEnd: ),
+        );
+  }
+}
